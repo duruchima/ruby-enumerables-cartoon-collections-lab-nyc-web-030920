e58a3e89315264d2bb79f9599e8abcd294805e97
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 18:22:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? { |word| word.length >= 4 }
+  array.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
