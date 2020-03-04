c7cbc16bf71176c974e3a2ce9ca90965007cbc86
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 18:00:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |value, index|
-    puts "#{index}. #{value}\n"
+    puts "#{index+1}. #{value}\n"
 end
 end
 
