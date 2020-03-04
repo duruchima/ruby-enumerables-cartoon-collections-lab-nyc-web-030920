33992880d6f3db87fdc715827bc173b1735ff79d
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 18:16:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,10 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+  i=0
   array.map do |word|
-    if word.length > 4 return true
-    else return false
+    return true if word.length > 4
+      else return false
     end
 end
 
