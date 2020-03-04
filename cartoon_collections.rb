7a7c5b4fefd7ed4b9bda72dd2ea0d7ab9e491d9f
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 17:57:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |value, index|
+    puts "#{index} #{value}"
 end
 
 def summon_captain_planet# code an argument here
